6adf8a3abe2e6db656efd6ea0a356f5dbbb8e68b
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-06-06 22:57:48 +0100

    create and style Home component

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
+import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-    const { error, isPending, data: blogs } = useFetch("https://localhost:8000/blogs");
+    const { error, isPending, data: blogs } = useFetch("http://localhost:8000/blogs");
+
+    return (
+        <div className="home">
+            { error && <div>{ error }</div> }
+            { isPending && <div>Loading...</div> }
+            { blogs && <BlogList blogs={blogs} /> }
+        </div>
+    );
 }
 
 export default Home;
